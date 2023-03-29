c5bd627e7518cc50a0b821ce887f6b9e3e0a98ae
Gautam Gupta <ggupta78@gmail.com>
2023-03-29 17:31:11 +0530

    add user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
+include ActionView::Helpers::UrlHelper
+
 class ApplicationController < ActionController::Base
   def hello
-    render html: 'Hello, World!'
+    render html: "Hello, World! <br> #{link_to 'Goto Users', users_path}".html_safe
   end
 end
