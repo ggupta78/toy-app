e1a1da1f9866d6dc805392c55145988f5ce78dcf
Gautam Gupta <ggupta78@gmail.com>
2023-03-29 17:45:44 +0530

    fix cyclic include error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
-include ActionView::Helpers::UrlHelper
+# include ActionView::Helpers::UrlHelper
 
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hello, World! <br> #{link_to 'Goto Users', users_path}".html_safe
+    # render html: "Hello, World! <br> #{link_to 'Goto Users', users_path}".html_safe
+    render html: 'Hello, World!'
   end
 end
