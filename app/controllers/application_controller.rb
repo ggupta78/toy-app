457f73876bffe97cdb53f793e9d7618311e6cff0
Gautam Gupta <ggupta78@gmail.com>
2023-03-30 16:49:05 +0530

    complete toy_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
-# include ActionView::Helpers::UrlHelper
 
 class ApplicationController < ActionController::Base
-  def hello
-    # render html: "Hello, World! <br> #{link_to 'Goto Users', users_path}".html_safe
-    render html: 'Hello, World!'
-  end
+  # include ActionView::Helpers::UrlHelper
+
+  # def hello
+  #   # render html: "Hello, World! <br> #{link_to 'Goto Users', users_path}".html_safe
+  #   # render html: 'Hello, World!'
+  # end
 end
